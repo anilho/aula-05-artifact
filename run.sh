ea9691f5a1a95cd80bf03c17b99f60490215c269
anilho <alicio@contaswap.com>
2024-09-03 20:47:27 -0300

    fix paths

--- run.sh
+++ run.sh
@@ -4,11 +4,7 @@ chmod +x ./hello-server
 
 sleep 5 & 
 
-echo "teste"
-
-
-
-# for LOGIN in Homer Bart Maggie;
-# do
-#     echo "$(date): $(curl -s http://localhost:13000/${LOGIN})"
-# done
+for LOGIN in Homer Bart Maggie;
+do
+    echo "$(date): $(curl -s http://localhost:13000/${LOGIN})"
+done
